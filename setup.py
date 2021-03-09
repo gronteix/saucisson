2e59e160da1481df908e3d15c0874f5b5705d4aa
Gustave  RONTEIX <gustave.ronteix@pasteur.fr>
2021-03-09 10:41:18 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="saucisson",
+    version="0.1",
+    description="Slice up your images",
+    author="Gustave Ronteix",
+    author_email="gronteix@pasteur.fr",
+    url="https://github.com/gronteix/saucisson",
+    install_requires=[
+        "numpy",
+        "os",
+        "math"
+    ]
+)
